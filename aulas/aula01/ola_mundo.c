5806050c2b34175d0b77589cef9b6077544c0bb1
Marlon Reis <marlon.reis@iesb.edu.br>
2025-02-26 21:42:55 -0300

    codigos da aula01

---
+++ aulas/aula01/ola_mundo.c
@@ -0,0 +1,14 @@
+/*
+Aula01 - Estrtura basica de
+um programa em C
+*/
+#include <stdio.h> // inclusao de bibliotecas
+
+// funcao principal que inicia o programa
+int main() {
+
+printf("Ola pessoal!\n");
+
+    return 0;
+}
+
